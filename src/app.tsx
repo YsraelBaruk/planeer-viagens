e9c69f699932f8659e4f90b05af8d1b986f1869b
israel <israekbaruque992@gmail.com>
2024-09-09 12:16:01 -0300

    file Invite Guests Modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CreateTripPage } from "./pages/create-trip";
+import { TripDetailsPage } from "./pages/trip-details";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <CreateTripPage/>,
   },
+  {
+    path: "/trips/:tripId",
+    element: <TripDetailsPage />,
+  }
 ]);
 
 export function App() {
   return(
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   )
 }
